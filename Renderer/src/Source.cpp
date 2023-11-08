866d5ac225199a43dde079869e54b240a7962ebd
Slaygony <rjs5785@hotmail.com>
2023-11-08 08:33:10 -0500

    device, device context, and swapchain are being created

--- Renderer/src/Source.cpp
+++ Renderer/src/Source.cpp
@@ -1,6 +1,16 @@
 #include "trpch.h"
 #include "Utilities/ErrorLogger.h"
 
+#include <d3d11.h>
+#include <dxgi.h>
+#include <dxgi1_2.h>
+#include <wrl/client.h>
+#include <d3dcompiler.h>
+
+#pragma comment(lib, "d3dcompiler.lib")
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "dxgi.lib")
+
 LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT Msg, WPARAM wParam, LPARAM lParam);
 
 
@@ -13,6 +23,11 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 
 	int width = 1000;
 	int height = 800;
+
+	Microsoft::WRL::ComPtr<ID3D11Device> d3dDevice;
+	Microsoft::WRL::ComPtr<ID3D11DeviceContext> d3ddeviceContext;
+	Microsoft::WRL::ComPtr<IDXGISwapChain1> dxgiswapChain;
+	Microsoft::WRL::ComPtr<IDXGIFactory2> dxgiFactory;
 
 	AllocConsole();
 	auto obj = freopen("CONIN$", "r", stdin);
@@ -29,15 +44,57 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	wc.lpfnWndProc = WindowProcedure;
 	RegisterClassEx(&wc);
 
+	// creating window and checking to make sure it succeeded
 	hwnd = CreateWindowEx(0, L"Renderer", L"Renderer", WS_OVERLAPPEDWINDOW, 0, 0, width, height, NULL, NULL, hInst, NULL);
 	if (hwnd == NULL)
+	{
 		Rhine::ErrorLogger::Log("Failed to create window handle");
-	ShowWindow(hwnd, SW_SHOW);
-	UpdateWindow(hwnd);
+		return -1;
+	}
+	else
+	{
+		ShowWindow(hwnd, SW_SHOW);
+		UpdateWindow(hwnd);
+	}
 
-	MSG msg = { 0 };
+	
+	DXGI_SWAP_CHAIN_DESC1 swapchainDescription;
+	ZeroMemory(&swapchainDescription, sizeof(DXGI_SWAP_CHAIN_DESC1));
+	swapchainDescription.BufferCount = 1;
+	swapchainDescription.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	swapchainDescription.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+	swapchainDescription.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
+	swapchainDescription.Width = width;
+	swapchainDescription.Height = height;
+	swapchainDescription.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+	swapchainDescription.SampleDesc.Count = 1;
+	swapchainDescription.SampleDesc.Quality = 0;
+
+
+	DXGI_SWAP_CHAIN_FULLSCREEN_DESC fullscreenDescription;
+	ZeroMemory(&fullscreenDescription, sizeof(DXGI_SWAP_CHAIN_FULLSCREEN_DESC));
+	fullscreenDescription.RefreshRate.Numerator = 60;
+	fullscreenDescription.RefreshRate.Denominator = 1;
+	fullscreenDescription.Windowed = true;
+	HRESULT hr = D3D11CreateDevice(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, D3D11_CREATE_DEVICE_DEBUG, 0, 0, D3D11_SDK_VERSION, d3dDevice.GetAddressOf(), 0, d3ddeviceContext.GetAddressOf());
+	if (FAILED(hr))
+	{
+		Rhine::ErrorLogger::Log(hr, "Failed to create Device");
+	}
+	
+	hr = CreateDXGIFactory(__uuidof(dxgiFactory), (void**)dxgiFactory.GetAddressOf());
+	if (FAILED(hr))
+	{
+		Rhine::ErrorLogger::Log(hr, "Failed to create factory");
+	}
+	hr = dxgiFactory->CreateSwapChainForHwnd(d3dDevice.Get(), hwnd, &swapchainDescription, &fullscreenDescription, 0, dxgiswapChain.GetAddressOf());
+	if (FAILED(hr))
+	{
+		Rhine::ErrorLogger::Log(hr, "Failed to create swap chain");
+	}
 	
 #pragma region application loops
+	MSG msg = { 0 };
 	/*while (true)
 	{
 		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
